8774d4063aacacc7b47ba7b879c47507080febbd
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-06-12 10:58:41 -0300

    Contas Update

--- src/Projeto_12_06/Contas/Conta.java
+++ src/Projeto_12_06/Contas/Conta.java
@@ -1,14 +1,26 @@
 package Projeto_12_06.Contas;
 
+import java.util.Random;
+
 public class Conta {
     String titular;
     String identificador;
+    String senha;
     float saldo;
+
+    public Conta(String titular, float saldo, String senha){
+        this.identificador = String.valueOf(new Random().nextInt(1000, 10000));
+        this.titular = titular;
+        this.saldo = saldo;
+        this.senha = senha;
+    }
+
     void depositar(float valor){
-        saldo+valor;
+        saldo += valor;
     }
     boolean sacar(float valor){
         if(valor <= saldo){
+            saldo -= valor;
             return true;
         }
         return false;
--- src/Projeto_12_06/Contas/Main.java
+++ src/Projeto_12_06/Contas/Main.java
@@ -3,29 +3,97 @@ package Projeto_12_06.Contas;
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.lang.Character.toLowerCase;
+import static java.lang.Character.toUpperCase;
+
 public class Main {
-    public static void main(String[] args){
 
-        Scanner ler = new Scanner(System.in);
+    static Scanner ler = new Scanner(System.in);
+    static Conta contas[] = new Conta[20];
+    static int contasQuntidade = 0;
 
-        System.out.println("-----CONTAS-----");
-        System.out.println("----------------");
-
-        Conta c1 = new Conta();
-
-        System.out.print("Nome: ");
-        c1.titular = ler.next();
-
+    public static void cadastro(){
+        System.out.println("---- CRIAR CONTA ----");
+        System.out.print("Titular: ");
+        String titular = ler.next();
         System.out.print("Saldo: ");
-        c1.saldo = ler.nextFloat();
+        float saldo = ler.nextFloat();
+        System.out.print("Senha: ");
+        String senha = ler.next();
+        Conta conta = new Conta(titular, saldo, senha);
+        contas[contasQuntidade] = conta;
+        mostrarConta(conta, true);
+        contasQuntidade++;
+    }
 
-        int identificador = new Random().nextInt(1000, 100000);
-        c1.identificador = String.valueOf(identificador);
+    public static void listar(){
+        for(int i=0; i<contasQuntidade; i++){
+            mostrarConta(contas[i]);
+        }
+    }
 
-        System.out.println("----------------");
-        System.out.println("--CONTA-CRIADA--");
-        System.out.println("Nome: " + c1.titular);
-        System.out.printf("Saldo: R$%.2f\n", c1.saldo);
-        System.out.println("Identificador: #" + c1.identificador);
+    public static void mostrarConta(Conta conta, boolean mostrarSenha){
+        System.out.println("---------------------");
+        System.out.println("Titular: " + conta.titular);
+        System.out.println("Saldo: " + String.format("R$%.2f", conta.saldo));
+        System.out.println("Indentificador: " + conta.identificador);
+        if(mostrarSenha)
+            System.out.println("Senha: " + conta.senha);
+    }
+
+    public static void depositar(){
+
+    }
+
+    public  static void sacar(){
+
+    }
+
+    public static void acessar(){
+        System.out.print("Identificador: ");
+        String identificador = ler.next();
+        System.out.print("Senha: ");
+        String senha = ler.next();
+        Conta conta;
+        for(int i=0; i<contasQuntidade; i++) {
+            if (contas[i].identificador.equals(identificador)) ;
+            conta = contas[i];
+        }
+    }
+
+    public static void main(String[] args){
+        boolean rodando = true;
+        do{
+            System.out.println("---------------------");
+            System.out.println("-------CONTAS--------");
+            System.out.println("---------------------");
+            System.out.println("1 - cadastrar conta");
+            System.out.println("2 - listar");
+            System.out.println("3 - depositar");
+            System.out.println("4 - sacar");
+            System.out.println("5 - sair");
+            System.out.print("Escolha: ");
+            char escolha = toLowerCase(ler.next().charAt(0));
+            if(escolha == '5')
+                rodando = false;
+            else{
+                switch(escolha){
+                    case '1':
+                        cadastro();
+                        break;
+                    case '2':
+                        listar();
+                        break;
+                    case '3':
+                        depositar();
+                        break;
+                    case '4':
+                        sacar();
+                        break;
+                    default:
+                        System.out.println("Escolha inválida");
+                }
+            }
+        }while(rodando);
     }
 }
