71d515a18984a28b808d838a53e0a66f8a3dcf3a
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-07-03 10:55:49 -0300

    Contas Update

--- src/Projeto_03_07/Conta_Corrente_Poupanca/ContaPoupanca.java
+++ src/Projeto_03_07/Conta_Corrente_Poupanca/ContaPoupanca.java
@@ -2,8 +2,6 @@ package Projeto_03_07.Conta_Corrente_Poupanca;
 
 public class ContaPoupanca extends Conta{
 
-    private float deposito;
-
     ContaPoupanca(String titular, String senha, float deposito){
         super(titular, senha);
         this.saldo = deposito;
