407d27044b6b753e52634bb7d8a1936ee74af27b
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-07-08 10:32:04 -0300

    Contas Update

--- src/Projeto_03_07/Conta_Corrente_Poupanca/Principal.java
+++ src/Projeto_03_07/Conta_Corrente_Poupanca/Principal.java
@@ -7,18 +7,18 @@ public class Principal {
     static boolean logado = false;
 
     static Scanner ler = new Scanner(System.in);
-    static Conta[] Contas = new Conta[20];
-    static int contC = 0;
+
     static ContaCorrente[] ContasCorrente = new ContaCorrente[20];
     static int contCC = 0;
     static ContaPoupanca[] ContasPoupanca = new ContaPoupanca[20];
     static int contCP = 0;
 
-    static Conta CLogada = new Conta();
-    static Conta CCLogada = new Conta();
-    static Conta CPLogada = new Conta();
+    static ContaCorrente CCLogada = new ContaCorrente();
+    static ContaPoupanca CPLogada = new ContaPoupanca();
 
-    public static String[] pegarTitularSenha(){
+    static String tipoConta = "";
+
+    static String[] pegarTitularSenha(){
         System.out.print("Digite o titular: ");
         String titular = ler.next();
         System.out.print("Digite sua senha: ");
@@ -27,29 +27,21 @@ public class Principal {
         return resp;
     }
 
-    public static void criarConta(){
+    static void criarConta(){
         char escolha = '0';
         do{
             System.out.println("+--------------------------+");
             System.out.println("|        CRIAR CONTA       |");
             System.out.println("+--------------------------+");
-            System.out.println("| 1 - Conta Padrão         |");
-            System.out.println("| 2 - Conta Corrente       |");
-            System.out.println("| 3 - Conta Poupanca       |");
-            System.out.println("| 4 - Cancelar             |");
+            System.out.println("| 1 - Conta Corrente       |");
+            System.out.println("| 2 - Conta Poupanca       |");
+            System.out.println("| 3 - Cancelar             |");
             System.out.println("+--------------------------+");
             System.out.print("Sua escolha: ");
             escolha = ler.next().charAt(0);
         }while(escolha != '1' && escolha != '2' && escolha != '3'  && escolha != '4');
         switch(escolha){
             case '1' ->{
-                String[] resp = pegarTitularSenha();
-                Conta c1 = new Conta(resp[0], resp[1]);
-                System.out.println("Seu identificador é: " + c1.getIdentificador());
-                Contas[contC] = c1;
-                contC++;
-            }
-            case '2' ->{
                 String[] resp = pegarTitularSenha();
                 System.out.print("Digite seu limite: ");
                 float limite = ler.nextFloat();
@@ -58,15 +50,16 @@ public class Principal {
                 ContasCorrente[contCC] = cc1;
                 contCC++;
             }
-            case '3' ->{
+            case '2' ->{
                 String[] resp = pegarTitularSenha();
+                System.out.print("Digite seu depósito: ");
                 float deposito = ler.nextFloat();
                 ContaPoupanca cp1 = new ContaPoupanca(resp[0], resp[1], deposito);
                 System.out.println("Seu identificador é: " + cp1.getIdentificador());
                 ContasPoupanca[contCP] = cp1;
                 contCP++;
             }
-            case '4' ->{
+            case '3' ->{
                 return;
             }
             default -> {
@@ -75,20 +68,10 @@ public class Principal {
         }
     }
 
-    public static void listarUsuarios(){
-        if(contC == 0 && contCC == 0 && contCP == 0){
+    static void listarUsuarios(){
+        if(contCC == 0 && contCP == 0){
             System.out.println("Ainda não há nenhuma conta.");
             return;
-        }
-        if(contC > 0){
-            System.out.println("+--------------------------+");
-            System.out.println("|      Contas Padrão       |");
-            System.out.println("+--------------------------+");
-            for(int i=0; i<contC; i++){
-                System.out.println("| Titular: " + ContasCorrente[i].getTitular() + " |" +
-                        " " + "Identificador: " + ContasCorrente[i].getIdentificador() +
-                        " |");
-            }
         }
         if(contCC > 0){
             System.out.println("+--------------------------+");
@@ -112,7 +95,7 @@ public class Principal {
         }
     }
 
-    public static boolean entrar(){
+    static boolean entrar(){
         System.out.println("+--------------------------+");
         System.out.println("|          Entrar          |");
         System.out.println("+--------------------------+");
@@ -120,25 +103,51 @@ public class Principal {
         String identificador = ler.next();
         System.out.print("Insira a senha da sua conta: ");
         String senha = ler.next();
-        for(int i=0; i<contC; i++){
-            if(Contas[i].validaAcesso(identificador, senha)){
-                CLogada = Contas[i];
-                return true;
-            }
-        }
         for(int i=0; i<contCC; i++){
             if(ContasCorrente[i].validaAcesso(identificador, senha)){
-                CCLogada = Contas[i];
+                CCLogada = ContasCorrente[i];
+                tipoConta = "CC";
                 return true;
             }
         }
         for(int i=0; i<contCP; i++){
-            if(Contas[i].validaAcesso(identificador, senha)){
-                CPLogada = Contas[i];
+            if(ContasPoupanca[i].validaAcesso(identificador, senha)){
+                CPLogada = ContasPoupanca[i];
+                tipoConta = "CP";
                 return true;
             }
         }
         return false;
+    }
+
+    static void sacar(){
+        System.out.println("+--------------------------+");
+        System.out.println("|          Saque           |");
+        System.out.println("+--------------------------+");
+        System.out.print("Digite o valor a ser sacado: ");
+        float valor = ler.nextFloat();
+        if(tipoConta.equals("CC")){
+            if(CCLogada.sacar(valor)){
+                System.out.println("Valor inválido.");
+            }
+            else{
+                System.out.println("Valor de R$" + String.format("%.2f", valor) + " " +
+                        "sacado.");
+            }
+        }
+        else if(tipoConta.equals("CP")){
+            if(CPLogada.sacar(valor)){
+                System.out.println("Valor inválido.");
+            }
+            else{
+                System.out.println("Valor de R$" + String.format("%.2f", valor) + " " +
+                        "sacado.");
+            }
+        }
+    }
+
+    static void depositar(){
+
     }
 
     public static void main(String[] args){
@@ -147,18 +156,38 @@ public class Principal {
 
         do{
             if(logado){
-                System.out.println("+--------------------------+");
-                System.out.println("|     SISTEMA BANCARIO     |");
-                System.out.println("+--------------------------+");
-                System.out.println("|                          |");
-                System.out.println("+--------------------------+");
-                System.out.println("| 1 - Sair                 |");
-                System.out.println("| 2 - Sacar                |");
-                System.out.println("| 3 - Depositar            |");
-                System.out.println("| 4 - Fechar               |");
-                System.out.println("+--------------------------+");
-                System.out.print("Sua escolha: ");
-                escolha = ler.next().charAt(0);
+                do{
+                    System.out.println("+--------------------------+");
+                    System.out.println("|     SISTEMA BANCARIO     |");
+                    System.out.println("+--------------------------+");
+                    System.out.println("|                          |");
+                    System.out.println("+--------------------------+");
+                    System.out.println("| 1 - Sair                 |");
+                    System.out.println("| 2 - Sacar                |");
+                    System.out.println("| 3 - Depositar            |");
+                    System.out.println("| S - Fechar               |");
+                    System.out.println("+--------------------------+");
+                    System.out.print("Sua escolha: ");
+                    escolha = ler.next().charAt(0);
+                }while(escolha != '1' && escolha != '2' && escolha != '3' && escolha != 'S');
+
+                switch(escolha){
+                    case '1' -> {
+                        logado = false;
+                    }
+                    case '2' -> {
+                        sacar();
+                    }
+                    case '3' -> {
+                        depositar();
+                    }
+                    case '4' -> {
+                        return;
+                    }
+                    default -> {
+                        throw new Error("Opção inválida: " + escolha);
+                    }
+                }
             }
             else{
                 do{
@@ -184,8 +213,11 @@ public class Principal {
                     case '3' -> {
                         listarUsuarios();
                     }
+                    case '4' -> {
+                        return;
+                    }
                     default -> {
-
+                        throw new Error("Opção inválida: " + escolha);
                     }
                 }
             }
